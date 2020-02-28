35407e33f6c54c4f17bf11ff96da13b0a8b83722
sandipan1 <h.sandipan05@gmail.com>
2020-02-28 18:57:38 +0530

    Added forms

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from django.http import HttpResponse, Http404
+from blog.forms import PostForm
+
 # Create your views here.
 
 def post_list(request):
@@ -9,9 +11,35 @@ def post_list(request):
 
     return render(request, 'blog/post_list.html',{'posts':posts})
 
-def post_detail(request, pk):
+def post_detail(request, pk=None):
     try:
         post = Post.objects.get(pk=pk)
     except Post.DoesnotExist:
         raise Http404('Page not found')
     return render(request, 'blog/post_detail.html', {'post': post})
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
